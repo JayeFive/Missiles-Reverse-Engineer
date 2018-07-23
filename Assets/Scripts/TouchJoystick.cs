aaf3adb4fb5dbdbd41d7096b273f3d7a88256f0f
John Olson <john.olson3@gmail.com>
2018-07-23 09:33:06 -0700

    Get axis from joystick controller

--- Assets/Scripts/TouchJoystick.cs
+++ Assets/Scripts/TouchJoystick.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class TouchJoystick : MonoBehaviour {
 
-    public GameObject Airplane;
-
+    public Vector3 joystickDirection = new Vector3();
 
 	// Use this for initialization
 	void Start () {
-		
+        joystickDirection.z = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-	}
+        float xpos = ETCInput.GetAxis("Horizontal");
+        float ypos = ETCInput.GetAxis("Vertical");
+        joystickDirection.x = xpos;
+        joystickDirection.y = ypos;
+    }
 
     void RotateAirplane ()
     {
