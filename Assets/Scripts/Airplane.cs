d4b66159f8f9418a4b2f5aa833bb51809116ab5f
John Olson <john.olson3@gmail.com>
2018-07-25 11:25:35 -0700

    Airplane constant motion as intended

--- Assets/Scripts/Airplane.cs
+++ Assets/Scripts/Airplane.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Airplane : MonoBehaviour {
 
-    public float AirplaneDirection;
+    public TouchJoystick joystick;
+    public float flightSpeed = 1.0f;
+    public float turnSpeed = 10.0f;
+    private float flightDirection;
+    private Vector3 rotation;
 
 	// Use this for initialization
 	void Start () {
-		
+
+        joystick = FindObjectOfType<TouchJoystick>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        transform.position += transform.up * flightSpeed * Time.deltaTime;
+
+        if (Input.GetMouseButton(0))
+        {
+            flightDirection = Mathf.Atan2(-joystick.joystickDirection.x, joystick.joystickDirection.y) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.Euler(0, 0, flightDirection), Time.deltaTime * turnSpeed);
+        }
+    }
 }
