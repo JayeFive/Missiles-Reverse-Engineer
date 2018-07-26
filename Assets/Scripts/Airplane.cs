9924fb17e68564a1c5c96d95a9b68e1e04695ca2
John Olson <john.olson3@gmail.com>
2018-07-26 11:15:22 -0700

    Add starting turn coroutine

--- Assets/Scripts/Airplane.cs
+++ Assets/Scripts/Airplane.cs
@@ -7,24 +7,46 @@ public class Airplane : MonoBehaviour {
     public TouchJoystick joystick;
     public float flightSpeed = 1.0f;
     public float turnSpeed = 10.0f;
+    public bool gameStarted = false;
+
     private float flightDirection;
     private Vector3 rotation;
 
 	// Use this for initialization
-	void Start () {
-
+	void Start ()
+    {
         joystick = FindObjectOfType<TouchJoystick>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
+	void Update ()
+    {
         transform.position += transform.up * flightSpeed * Time.deltaTime;
 
-        if (Input.GetMouseButton(0))
+        if (gameStarted)
         {
-            flightDirection = Mathf.Atan2(-joystick.joystickDirection.x, joystick.joystickDirection.y) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.Euler(0, 0, flightDirection), Time.deltaTime * turnSpeed);
+            if (Input.GetMouseButton(0))
+            {
+                flightDirection = Mathf.Atan2(-joystick.joystickDirection.x, joystick.joystickDirection.y) * Mathf.Rad2Deg;
+                Debug.Log(flightDirection);
+                TurnAirplane(flightDirection);
+            }
         }
+    }
+
+    public IEnumerator StartingTurn ()
+    {
+        for (float flightDirection = 0; flightDirection <= 180; flightDirection += Time.deltaTime * turnSpeed)
+        {
+            TurnAirplane(flightDirection);
+            yield return null;
+        }
+
+        gameStarted = true;
+    }
+
+    void TurnAirplane (float flightDirection)
+    {
+        transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.Euler(0, 0, flightDirection), Time.deltaTime * turnSpeed);
     }
 }
