572fef25293d48d11eb7febf49a844618535c0f3
John Olson <john.olson3@gmail.com>
2018-07-26 11:15:45 -0700

    Add gameplay script for handing game events and animations

---
+++ Assets/Scripts/GamePlay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlay : MonoBehaviour {
+
+    private Airplane airplane;
+    public TouchJoystick joystick;
+    bool gameStarted = false;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        airplane = FindObjectOfType<Airplane>();
+        joystick = FindObjectOfType<TouchJoystick>();
+
+        //airplane.StartingTurn();
+        airplane.StartCoroutine("StartingTurn");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (airplane.gameStarted)
+        {
+            joystick.GetComponent<ETCJoystick>().visible = true;
+        }
+
+    }
+}
