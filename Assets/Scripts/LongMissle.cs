b25e73f9c752903a6e789ad1bed68de5aa76742c
John Olson <john.olson3@gmail.com>
2018-08-01 16:11:54 -0700

    refactor

--- Assets/Scripts/LongMissle.cs
+++ Assets/Scripts/LongMissle.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class LongMissle : MonoBehaviour {
 
-    private MissleController missleController;
-
     [SerializeField] float flightSpeed;
     [SerializeField] float turnSpeed;
     [SerializeField] float lifeSpan;
+
+    private MissleController missleController;
 
     // Use this for initialization
     void Start()
     {
         missleController = GetComponent<MissleController>();
+        SetMissleProperties();
+    }
 
+    private void SetMissleProperties()
+    {
         missleController.flightSpeed = flightSpeed;
         missleController.turnSpeed = turnSpeed;
         missleController.lifeSpan = lifeSpan;
