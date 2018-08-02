a0ee85564a73799fe79e7bde2d629c034f15f651
John Olson <john.olson3@gmail.com>
2018-08-01 22:52:03 -0700

    add basic missle launching script

--- Assets/Scripts/MissleSpawner.cs
+++ Assets/Scripts/MissleSpawner.cs
@@ -11,25 +11,12 @@ public class MissleSpawner : MonoBehaviour {
     public Airplane airplane;
     public GameObject longMissle;
 
+    MediumMissleTriple mediumMissleTriple;
 
     void Start ()
     {
-        airplane = FindObjectOfType<Airplane>();
-        Invoke("SpawnMissle", 2);
-    }
+        mediumMissleTriple = GetComponent<MediumMissleTriple>();
 
-    void Update ()
-    {
-
-    }
-
-    public void SpawnMissle()
-    {
-        Vector3 spawnLocation = airplane.transform.position + topSpawnLocation;
-        Vector2 vectorToAirplane = airplane.transform.position - spawnLocation;
-        float angle = Mathf.Atan2(vectorToAirplane.y, vectorToAirplane.x) * Mathf.Rad2Deg;
-        Quaternion startingRotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-        GameObject newMissle = Instantiate(longMissle, spawnLocation, startingRotation);
+        StartCoroutine(mediumMissleTriple.StartLaunchController());
     }
 }
---
+++ Assets/Scripts/MissleSpawns/MediumMissleTriple.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MediumMissleTriple : MonoBehaviour {
+
+    const int numMissles = 3;
+
+    private Airplane airplane;
+    public GameObject mediumMissle;
+
+    [SerializeField] Vector2 spawnOffset;
+    [SerializeField] float horizontalRangeFloor;
+    [SerializeField] float horizontalRangeCeiling;
+    [SerializeField] float timeToLaunch;
+    [SerializeField] float timeBetweenFloor;
+    [SerializeField] float timeBetweenCeiling;
+
+    private float timeBetweenMissles = 0.0f;
+
+    void Start ()
+    {
+        airplane = FindObjectOfType<Airplane>();
+    }
+
+    public IEnumerator StartLaunchController ()
+    {
+        yield return new WaitForSeconds(timeToLaunch);
+
+        for (int i = 0; i < numMissles; i++)
+        {
+            StartCoroutine(LaunchMissle(timeBetweenMissles));
+            timeBetweenMissles += Random.Range(timeBetweenFloor, timeBetweenCeiling);
+        }
+    }
+
+    private IEnumerator LaunchMissle (float t)
+    {
+        yield return new WaitForSeconds(t);
+
+        var horizontalOffset = RandomizeHorizontal(spawnOffset);
+
+        Vector3 spawnLocation = airplane.transform.position + horizontalOffset;
+        Vector2 vectorToAirplane = airplane.transform.position - spawnLocation;
+        float angle = Mathf.Atan2(vectorToAirplane.y, vectorToAirplane.x) * Mathf.Rad2Deg;
+        Quaternion startingRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        GameObject newMissle = Instantiate(mediumMissle, spawnLocation, startingRotation);
+        Debug.Log("Missle spawned");
+    }
+
+    private Vector3 RandomizeHorizontal (Vector3 offset)
+    {
+        var xPos = Random.Range(horizontalRangeFloor, horizontalRangeCeiling);
+        offset.x += xPos;
+
+        return offset;
+    }
+}
+
