8812e22361a5f109e1e11933989e3d86b1d017f1
John Olson <john.olson3@gmail.com>
2018-08-14 09:58:46 -0700

    destroy smoke trail after it dissapears

---
+++ Assets/Scripts/smokeTrail.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smokeTrail : MonoBehaviour {
+
+	void Update ()
+    {
+	    if (transform.parent == null)
+        {
+            CheckForEnd();
+        }	
+	}
+
+    private void CheckForEnd ()
+    {
+        if (gameObject.GetComponent<ParticleSystem>().particleCount == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
