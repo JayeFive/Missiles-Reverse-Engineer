7778c906d75c99652e03d81122d167bece732b8d
John Olson <john.olson3@gmail.com>
2018-08-30 19:36:01 -0700

    add star counter functionality

---
+++ Assets/Scripts/StarBonus.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarBonus : MonoBehaviour {
+
+    private Text starScore;
+    private GamePlay gamePlay;
+
+	// Use this for initialization
+	void Start ()
+    {
+        starScore = GameObject.Find("StarCounterText").GetComponent<Text>();
+        gamePlay = FindObjectOfType<GamePlay>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.gameObject.tag == "Airplane")
+        {
+            gamePlay.starScore++;
+            starScore.GetComponent<Text>().text = gamePlay.starScore.ToString();
+
+            Destroy(gameObject);
+        }
+    }
+}
