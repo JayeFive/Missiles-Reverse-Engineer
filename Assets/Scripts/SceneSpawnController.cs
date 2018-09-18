cea97e81b5b610689152767bf2f13bda6a717c77
John Olson <john.olson3@gmail.com>
2018-09-18 12:08:14 -0700

    Add spawn locations for star bonuses. Add editor read only field of arrangement type for sceneSpawnController instances

--- Assets/Scripts/SceneSpawnController.cs
+++ Assets/Scripts/SceneSpawnController.cs
@@ -1,35 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class SceneSpawnController : MonoBehaviour
 {
     // Designer fields
-    [SerializeField] private string type = "";
+    [SerializeField][ReadOnly] private string type = "";
     [SerializeField] private int maxSceneWeight = 0;
     [SerializeField] private float spawnTimerBase = 0.0f;
     [SerializeField] private float spawnTimerVariation = 0.0f;
+    [SerializeField] private float spawnMagnitude = 0.0f;
     [SerializeField] private Arrangement[] arrangements = new Arrangement[0];
 
     // Logic fields
     private List<GameObject> currentSceneArrangements = new List<GameObject>();
 
-    void Start ()
+    void Start()
     {
-        if (arrangements.Length > 0)
-        {
-            type = arrangements[0].gameObject.tag;
-        } 
-        else
-        {
-            type = "No arrangements found";
-        }
+        SetTypeInEditor();
 
         StartCoroutine(Spawn());
     }
 
+    private void SetTypeInEditor ()
+    {
+        if (arrangements.Length > 0)
+        {
+            type = CheckArrangementType();
+        }
+        else
+        {
+            type = "No arrangements found!";
+        }
+    }
+
+    private string CheckArrangementType ()
+    {
+        string _type = arrangements[0].tag;
+
+        foreach(Arrangement arrangement in arrangements)
+        {
+            if (arrangement.tag != _type)
+            {
+                return "Incompatable arrangement types!";
+            }
+        }
+
+        return _type;
+    }
+
     // Spawning Coroutine
-    private IEnumerator Spawn ()
+    private IEnumerator Spawn()
     {
         yield return new WaitForSeconds(Random.Range(spawnTimerBase - spawnTimerVariation, spawnTimerBase + spawnTimerVariation));
 
@@ -38,15 +60,15 @@ public class SceneSpawnController : MonoBehaviour
         if (spawnableArrangements.Count > 0)
         {
             var arrangementToSpawn = WeightedSpawner.GetChanceArrangement(spawnableArrangements);
-            Instantiate(arrangementToSpawn, GetSpawnLoc(), Quaternion.identity);
+            Instantiate(arrangementToSpawn, GetSpawnLoc(arrangementToSpawn.GetComponent<Arrangement>()), Quaternion.identity);
             currentSceneArrangements.Add(arrangementToSpawn);
         }
 
         StartCoroutine(Spawn());
     }
-    
+
     // Determine spawnable arrangement list
-    private List<Arrangement> SpawnableArrangements ()
+    private List<Arrangement> SpawnableArrangements()
     {
         List<Arrangement> spawnableArrangements = new List<Arrangement>();
         var currentSceneWeight = CurrentSceneWeight();
@@ -62,12 +84,12 @@ public class SceneSpawnController : MonoBehaviour
         return spawnableArrangements;
     }
 
-    private float CurrentSceneWeight ()
+    private float CurrentSceneWeight()
     {
         int currentSceneWeight = 0;
         Arrangement[] sceneArrangements = FindObjectsOfType<Arrangement>();
 
-        foreach(Arrangement sceneArrangement in sceneArrangements)
+        foreach (Arrangement sceneArrangement in sceneArrangements)
         {
             if (sceneArrangement.gameObject.tag == type)
             {
@@ -81,8 +103,36 @@ public class SceneSpawnController : MonoBehaviour
 
     // Location determination to be moved later
     // For testing, spawns center screen
-    private Vector2 GetSpawnLoc()
+    private Vector2 GetSpawnLoc(Arrangement arrangement)
     {
-        return Camera.main.transform.position;
+        return SpawnLocations.GetSpawnLocation(arrangement, spawnMagnitude);
+    }
+
+    public class ReadOnlyAttribute : PropertyAttribute
+    {
+
+    }
+
+
+
+
+    /* This custom drawer is used to show the arrangemnt type for each instance of this class in the editor for readability */
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property,
+                                                GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+
+        public override void OnGUI(Rect position,
+                                   SerializedProperty property,
+                                   GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
+        }
     }
 }
---
+++ Assets/Scripts/SpawnLocations.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class SpawnLocations
+{
+    public static Vector2 GetSpawnLocation (Arrangement arrangement, float spawnMagnitude)
+    {
+        switch (arrangement.tag)
+        {
+            case "StarBonus":
+                return StarBonusLoc(spawnMagnitude);
+            case "PowerUp":
+                break;
+            case "Missile":
+                break;
+            default:
+                Debug.Log("arrangement tag not found in SpawnLocations.cs");
+                break;
+        }
+
+        return new Vector2(0, 0);
+    }
+
+    private static Vector2 StarBonusLoc (float spawnMagnitutde)
+    {
+        var airplanePos = (Vector2)GameObject.FindObjectOfType<Airplane>().transform.position;
+
+        var spawnLoc = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 0.0f)).normalized;
+
+        return airplanePos + (spawnLoc * spawnMagnitutde);
+    }
+}
+
