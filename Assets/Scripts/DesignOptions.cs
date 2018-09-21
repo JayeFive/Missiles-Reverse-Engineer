1139c0de3431bd7413a41e6161af2ce4396e4a70
John Olson <john.olson3@gmail.com>
2018-09-20 19:11:00 -0700

    generic bucket for global positioning values

---
+++ Assets/Scripts/DesignOptions.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesignOptions : MonoBehaviour
+{
+    public float fractionOfIndicatorFromEdge = 0.0f;
+}
