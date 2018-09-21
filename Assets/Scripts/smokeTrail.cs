768bfa43d8a88728b954401d009709158c9f9dcd
John Olson <john.olson3@gmail.com>
2018-09-21 12:54:33 -0700

    fix sputter out effect with particle emitter
    Also gives intervals of 3 short burts every 1.3 seconds(editable in inspector), which is closer to the original design. I might also split up the sputter routine and have it emit even less particles during the second half to really send home the visual cue of its end.

--- Assets/Scripts/smokeTrail.cs
+++ Assets/Scripts/smokeTrail.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class smokeTrail : MonoBehaviour {
 
-    [SerializeField] float sputterCurveMax;
+    [SerializeField] int burstCount;
+    [SerializeField] float timeBetweenBursts;
 
     private ParticleSystem smokeParticles;
-    private ParticleSystem.EmissionModule emissionModule; 
+    private ParticleSystem.EmissionModule emissionModule;
+
+    // Bursts
 
     void Start ()
     {
@@ -25,7 +29,15 @@ public class smokeTrail : MonoBehaviour {
 
     public void SputterTrail ()
     {
-        emissionModule.rateOverDistance = new ParticleSystem.MinMaxCurve(0.0f, sputterCurveMax);
+        emissionModule.rateOverDistance = 0;
+        emissionModule.burstCount = burstCount;
+
+        for (int i = 0; i < burstCount; i++)
+        {
+            var burst = new ParticleSystem.Burst(i * 0.1f, 1, 1, 0, timeBetweenBursts);
+            emissionModule.SetBurst(i, burst);
+        }
+
     }
 
     private void CheckForEnd ()
