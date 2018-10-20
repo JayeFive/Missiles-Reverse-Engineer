55e91deb353ecd36132d37553e5f363d2e4eacbb
John Olson <john.olson3@gmail.com>
2018-10-20 16:06:26 -0700

    add bonus counter to UI on missile collisions

--- Assets/Scripts/GamePlay.cs
+++ Assets/Scripts/GamePlay.cs
@@ -2,31 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using Assets.UIExtensions;
 
 public class GamePlay : MonoBehaviour {
 
+    [SerializeField] private float bonusFadeSpeed = 0.0f;
+
     private Airplane airplane;
     public TouchJoystick joystick;
-    private Text starScoreText;
+    private Text starsText;
+    private Text bonusText;
 
-    private int starScore = 0;
+    private int stars = 0;
+    private int bonus = 0;
 
-    public int StarScore
+public int Stars
     {
-        get { return starScore; }
+        get { return stars; }
         set
         {
-            starScore = value;
-            starScoreText.GetComponent<Text>().text = starScore.ToString();
+            stars = value;
+            if (starsText != null) starsText.GetComponent<Text>().text = stars.ToString();
         }
     }
 
-	// MonoBehavior
-	void Start ()
+    public int Bonus
+    {
+        get { return bonus; }
+        set
+        {
+            bonus += value;
+            if (bonusText != null) StartCoroutine(ShowBonus(value));
+        }
+    }
+
+    // MonoBehavior
+    void Start ()
     {
         airplane = FindObjectOfType<Airplane>();
         joystick = FindObjectOfType<TouchJoystick>();
-        starScoreText = GameObject.Find("StarCounterText").GetComponent<Text>();
+        starsText = GameObject.Find("StarCounterText").GetComponent<Text>();
+        bonusText = GameObject.Find("BonusText").GetComponent<Text>();
+
 
         // TODO create coroutine utility class to return bool value from this coroutine
         // https://answers.unity.com/questions/24640/how-do-i-return-a-value-from-a-coroutine.html
@@ -43,6 +60,17 @@ public class GamePlay : MonoBehaviour {
     }
 
 
+    // UI Methods
+
+    private IEnumerator ShowBonus(float scoreAddition)
+    {
+        bonusText.text = "+" + scoreAddition.ToString();
+        StartCoroutine(bonusText.CrossFadeAlphaFixed(1f, bonusFadeSpeed));
+
+        yield return new WaitForSeconds(3f);
+
+        StartCoroutine(bonusText.CrossFadeAlphaFixed(0f, bonusFadeSpeed));
+    }
 
     public void ShowResetUI ()
     {
---
+++ Assets/Scripts/GraphicExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace Assets.UIExtensions
+{
+    public static class GraphicExtensions
+    {
+        // Fade methods for UI elements
+
+        //public static void FadeIn(this Graphic g)
+        //{
+        //    g.canvasRenderer.SetAlpha(0.0f);
+        //    g.CrossFadeAlpha(1.0f, 3.0f, false);
+        //}
+
+        //public static void FadeOut(this Graphic g)
+        //{
+        //    g.canvasRenderer.SetAlpha(1.0f);
+        //    g.CrossFadeAlpha(0.0f, 3.0f, false);
+        //}
+
+        public static IEnumerator CrossFadeAlphaFixed(this Graphic elem, float alpha, float duration)
+        {
+            Color currentColor = elem.color;
+
+            Color visibleColor = elem.color;
+            visibleColor.a = alpha;
+
+            float counter = 0;
+
+            while (counter < duration)
+            {
+                counter += Time.deltaTime;
+                elem.color = Color.Lerp(currentColor, visibleColor, counter / duration);
+                yield return null;
+            }
+        }
+    }
+}
