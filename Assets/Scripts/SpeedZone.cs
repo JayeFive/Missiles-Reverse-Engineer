bb656c0bdc1d49cd072cf510794d8af3b18359df
John Olson <john.olson3@gmail.com>
2018-10-20 16:08:22 -0700

    area to slow missiles down to game speed

---
+++ Assets/Scripts/SpeedZone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.gameObject.tag == "Missile")
+        {
+            other.gameObject.GetComponent<Missile>().HasEnteredSpeedZone = true;
+        }
+    }
+
+    private void OnTriggerExit2D (Collider2D other)
+    {
+        if (other.gameObject.tag == "Missile")
+        {
+            if (other.gameObject.GetComponent<Missile>().IsRepeatableSpeedZone)
+            {
+                other.gameObject.GetComponent<Missile>().HasEnteredSpeedZone = false;
+            }
+        }
+    }
+}
