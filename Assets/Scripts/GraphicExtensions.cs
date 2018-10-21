70dfd13f8b6def8b221413185251c7ca07cd7d35
John Olson <john.olson3@gmail.com>
2018-10-21 13:58:47 -0700

    refactor

--- Assets/Scripts/GraphicExtensions.cs
+++ Assets/Scripts/GraphicExtensions.cs
@@ -8,32 +8,23 @@ namespace Assets.UIExtensions
     {
         // Fade methods for UI elements
 
-        //public static void FadeIn(this Graphic g)
-        //{
-        //    g.canvasRenderer.SetAlpha(0.0f);
-        //    g.CrossFadeAlpha(1.0f, 3.0f, false);
-        //}
 
-        //public static void FadeOut(this Graphic g)
-        //{
-        //    g.canvasRenderer.SetAlpha(1.0f);
-        //    g.CrossFadeAlpha(0.0f, 3.0f, false);
-        //}
-
-        public static IEnumerator CrossFadeAlphaFixed(this Graphic elem, float alpha, float duration)
+        public static IEnumerator CrossFadeAlphaFixed(this Graphic elem, float alpha, float duration = 1.0f)
         {
-            Color currentColor = elem.color;
+            var startTime = Time.time;
+            var timeSinceStart = Time.time - startTime;
+            var lerpPercentage = timeSinceStart / duration;
 
+            Color currentColor = elem.color;
             Color visibleColor = elem.color;
             visibleColor.a = alpha;
 
-            float counter = 0;
-
-            while (counter < duration)
+            while (lerpPercentage <= 1f)
             {
-                counter += Time.deltaTime;
-                elem.color = Color.Lerp(currentColor, visibleColor, counter / duration);
-                yield return null;
+                timeSinceStart = Time.time - startTime;
+                lerpPercentage = timeSinceStart / duration;
+                elem.color = Color.Lerp(currentColor, visibleColor, lerpPercentage);
+                yield return new WaitForEndOfFrame();
             }
         }
     }
