9ba478284c176e81521869ae7f3207b4147a3f21
John Olson <john.olson3@gmail.com>
2018-10-27 09:26:06 -0700

    Initial script loading

---
+++ Assets/Scripts/LoadInit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LoadInit : MonoBehaviour
+{
+    public GameObject gameManager;
+
+    //MonoBehavior
+    void Awake()
+    {
+        if (GameManager.instance == null)
+        {
+            Instantiate(gameManager);
+        }
+    }
+}
