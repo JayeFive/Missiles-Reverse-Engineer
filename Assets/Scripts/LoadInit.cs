8fc2ccdf4a89fa5ecab98dcb8a0aea5aa75fbfe5
John Olson <john.olson3@gmail.com>
2018-10-29 15:48:19 -0700

    GameManager instance name change

--- Assets/Scripts/LoadInit.cs
+++ Assets/Scripts/LoadInit.cs
@@ -8,7 +8,7 @@ public class LoadInit : MonoBehaviour
     //MonoBehavior
     void Awake()
     {
-        if (GameManager.instance == null)
+        if (GameManager.Instance == null)
         {
             Instantiate(gameManager);
         }
