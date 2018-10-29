cf8f995aa9cc2523d3b50435778109db2a6f99bc
John Olson <john.olson3@gmail.com>
2018-10-29 15:50:45 -0700

    UIManager to hold all UI functions

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Canvas HUD;
+    public Canvas HomeScreen;
+    public Canvas GameOverScreen;
+
+    public void DisplayStarScore(int value)
+    {
+        var starScore = HUD.transform.Find("StarScoreText");
+
+        if (starScore != null)
+        {
+            starScore.GetComponent<Text>().text = value.ToString();
+        }
+        else
+        {
+            Debug.LogWarning("[UIManager] StarScoreText not found in HUD");
+        }
+    }
+}
